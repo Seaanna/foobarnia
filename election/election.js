aca3fc4476fc6f57c8d4e55c66571904d5096f03
Seaanna <seaanna.thompson@gmail.com>
2018-05-12 19:56:18 -0700

    Check runoff for ties and added comments

--- election/election.js
+++ election/election.js
@@ -1,32 +1,43 @@
 exports.tally = function(votes) {
   let tallyResult= [];
   let counts = [];
-  const majorityVote = parseInt(votes.length/2) + 1;
 
-  for (let i = 0; i < votes.length; i++) {
-    const number = votes[i];
-    const count = counts[number] ? counts[number] + 1 : 1;
+  // get frequency of votes and store result in counts
+  for (let index = 0; index < votes.length; index++) {
+    const number = votes[index];
 
     let existingIndex = counts.findIndex(count => count.key == number);
     if (existingIndex >= 0) {
       counts[existingIndex].votes += 1;
     } else {
-        counts.push({key: number, votes: 1})
+        counts.push({key: number, votes: 1});
     }
   }
 
+  // sorts count greatest to smallest
   const sortedCounts = counts.sort(function(a,b) {
     return b.votes - a.votes;
   });
 
-  if (sortedCounts[0].votes >= majorityVote) {
+  // check for majority winner
+  const majorityThreshold = parseInt(votes.length/2) + 1;
+  const majorityWinner = sortedCounts[0].votes >= majorityThreshold;
+  if (majorityWinner) {
     tallyResult = [sortedCounts[0].key]
   } else {
+    // election runoff
     tallyResult = [
       sortedCounts[0].key,
       sortedCounts[1].key,
       sortedCounts[2].key
-    ].sort()
+    ]
+    // check runoff for ties
+    let index = 3;
+    while (sortedCounts[2].votes == sortedCounts[index].votes){
+      tallyResult.push(sortedCounts[index].key);
+      index++;
+    }
+    tallyResult.sort();
   }
 
   return tallyResult;
