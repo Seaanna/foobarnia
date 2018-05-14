4de2bf9364f1c089a7dfa7cfe1117edbfa49dde9
Seaanna <seaanna.thompson@gmail.com>
2018-05-13 18:15:52 -0700

    Update filter function to handle multiple filter types

--- mileage/mileage.rb
+++ mileage/mileage.rb
@@ -2,13 +2,22 @@ require 'csv'
 require './auto_seeker'
 require 'pry'
 
+# this script returns median mileage based on filter arguments
+# argument 0 should be filter type 
+# argument 1 should be filter value
+
 data = CSV.read('foobarnian_autos.csv')
 
+# check first argument for filter
+unless ["color", "fuel", "price"].include? ARGV[0]
+  abort "You did not pass in the correct argument. #{ARGV[0]} must be color, fuel, or price."
+end
+
 seeker = AutoSeeker.new data
-autos = seeker.filter(:color, ARGV[0])
+autos = seeker.filter(ARGV[0], ARGV[1])
 
 if autos.length == 0
-  abort "no autos with color #{ARGV[0]} found"
+  abort "no autos found."
 end
 
 mileage = AutoSeeker.median_mileage(autos)
